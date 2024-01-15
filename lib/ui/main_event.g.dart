9931d45ce7a95e5cf760477f20bcab70ed4027e1
ssd72 <sungdae.yi@gmail.com>
2024-01-15 12:46:58 +0100

    24년 1월 12일 클론코딩 이미지 찾기 - 0:58:23 소요 (snackBar, dialog event 구현까지 지난번보다 -3분)

---
+++ lib/ui/main_event.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main_event.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ShowSnackBarImpl _$$ShowSnackBarImplFromJson(Map<String, dynamic> json) =>
+    _$ShowSnackBarImpl(
+      json['message'] as String,
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$ShowSnackBarImplToJson(_$ShowSnackBarImpl instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+      'runtimeType': instance.$type,
+    };
+
+_$ShowDialogImpl _$$ShowDialogImplFromJson(Map<String, dynamic> json) =>
+    _$ShowDialogImpl(
+      json['message'] as String,
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$ShowDialogImplToJson(_$ShowDialogImpl instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+      'runtimeType': instance.$type,
+    };
